56b85fd341b51628fa1fd94a81b312142655b018
BradenFish <braden.j.fisher-1@ou.edu>
2023-11-08 11:43:30 -0600

    post 11/08 lecture

--- MIS3033_LC_1108_BradenFisher/Controllers/HomeController.cs
+++ MIS3033_LC_1108_BradenFisher/Controllers/HomeController.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MIS3033_LC_1108_BradenFisher.Data;
+using MIS3033_LC_1108_BradenFisher.Models;
 
 namespace MIS3033_LC_1108_BradenFisher.Controllers
 {
     public class HomeController : Controller
     {
         StuDB db = new StuDB();
+
+        public JsonResult AddStu(string id, string name, DateTime dob)
+        {
+            Student stu;// complex
+            stu = new Student();
+            stu.Id = id;    
+            stu.Name = name;
+            //stu.DOB = dob.ToLocalTime();// 
+            stu.DOB = dob.ToUniversalTime();// 
+
+            db.Students.Add(stu);
+            db.SaveChanges();
+
+            Message mes;// complex
+            mes = new Message();
+
+            return Json(mes);
+        }
+
         public JsonResult GetStus()
         {
             var r = db.Students;
